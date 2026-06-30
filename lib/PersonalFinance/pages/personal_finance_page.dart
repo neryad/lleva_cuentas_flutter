6857b12a94b435f76b20f96ce5a625add048f6c7
fff <ffff2gmail.com>
2026-06-30 10:09:34 -0400

    feat: add bottom navigation bar to home screen

---
+++ lib/PersonalFinance/pages/personal_finance_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class PersonalFinancePage extends StatelessWidget {
+  const PersonalFinancePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Finanzas Personales - Próximamente'),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,63 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:lleva_cuentas/Home/pages/home_page.dart';
-import 'package:lleva_cuentas/about/pages/about.dart';
-import 'package:lleva_cuentas/Settings/pages/settings_page.dart';
-import 'package:lleva_cuentas/Settings/pages/legal_page.dart';
-import 'package:lleva_cuentas/theme_manager.dart';
 import 'package:provider/provider.dart';
+import 'theme_manager.dart';
+import 'Home/pages/home_page.dart';
+import 'PersonalFinance/pages/personal_finance_page.dart';
+import 'Settings/pages/settings_page.dart';
+import 'Settings/pages/legal_page.dart';
+import 'about/pages/about.dart';
 
-// void main() {
-//   runApp(const MyApp());
-// }
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({Key? key}) : super(key: key);
-
-//   // This widget is the root of your application.
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       //theme: ThemeData(colorSchemeSeed: Color(0xff1e234b)),
-//       title: 'Llevacuentas',
-//       home: const HomePage(),
-//       routes: {
-//         'home': (BuildContext context) => const HomePage(),
-//         // 'details': (BuildContext context) => const DetailsPage(),
-//         // 'amount': (BuildContext context) => const AmountPage()
-//       },
-//     );
-//   }
-// }
-// void main() async {
-//   WidgetsFlutterBinding.ensureInitialized();
-//   final themeManager = ThemeManager();
-//   themeManager.loadTheme(); // Esperar a que cargue las preferencias
-
-//   runApp(
-//     ChangeNotifierProvider(
-//       create: (_) => themeManager,
-//       child: const MyApp(),
-//     ),
-//   );
-// }
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Consumer<ThemeManager>(
-//       builder: (context, themeManager, _) {
-//         return MaterialApp(
-//           title: 'Lleva Cuentas',
-//           debugShowCheckedModeBanner: false,
-//           theme: themeManager.getThemeData(),
-//           home: const HomePage(),
-//         );
-//       },
-//     );
-//   }
-// }
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   final themeManager = ThemeManager();
@@ -81,15 +30,57 @@ class MyApp extends StatelessWidget {
           title: 'Lleva Cuentas',
           debugShowCheckedModeBanner: false,
           theme: themeManager.getThemeData(),
-          initialRoute: '/',
+          home: const MainScreen(),
           routes: {
-            '/': (context) => const HomePage(),
             '/settings': (context) => const SettingsPage(),
             '/legal': (context) => const LegalPage(),
             '/about': (context) => const AboutPage(),
           },
         );
       },
+    );
+  }
+}
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _selectedIndex = 0;
+
+  final List<Widget> _pages = [
+    const PersonalFinancePage(),
+    const HomePage(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: NavigationBar(
+        selectedIndex: _selectedIndex,
+        onDestinationSelected: (index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
+        destinations: const [
+          NavigationDestination(
+            icon: Icon(Icons.account_balance_wallet),
+            selectedIcon: Icon(Icons.account_balance_wallet, color: Colors.green),
+            label: 'Finanzas',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.list_alt),
+            selectedIcon: Icon(Icons.list_alt, color: Colors.blue),
+            label: 'Cuentas',
+          ),
+        ],
+      ),
     );
   }
 }
